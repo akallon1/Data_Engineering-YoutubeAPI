2660b8da57bab2e253fd6f6b04e4ac51f1482c27
Alpha Kallon <akallon@deloitte.dk>
2024-03-19 17:30:48 +0100

    youtube end to end api

---
+++ Channelstats.py
@@ -0,0 +1,34 @@
+import Extract
+from Extract import *
+
+channel_ids = ['UCDWnJMuJXwnMG9Pm7ERoVYA',]
+
+yt = Extract.Youtube()
+
+
+
+request = yt.channels().list(
+    part="snippet,contentDetails,statistics",
+    id=','.join(channel_ids)
+)
+response = request.execute()
+
+
+# Loop through items
+def channel_stats(youtube,channel_ids):
+    all_data = []
+    for item in response['items']:
+        data = {
+            'channelname': item['snippet']['title'],
+            'channelStartDate': item['snippet']['publishedAt'],
+            'Country': item['snippet']['country'],
+            'subscribers': item['statistics']['subscriberCount'],
+            'views': item['statistics']['viewCount'],
+            'TotalVidoes': item['statistics']['videoCount'],
+            'PlayListID': item['contentDetails']['relatedPlaylists']['uploads']
+        }
+        all_data.append(data)
+    return pd.DataFrame(all_data)
+
+channel_stats_df = channel_stats(yt,channel_ids)
+channel_stats_df.to_csv('Channel_Details.csv', index=False)
---
+++ Comments.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import PlaylistItems
+from PlaylistItems import *
+import Extract
+from Extract import *
+
+yt = Extract.Youtube()
+video_ids = PlaylistItems.video_stats(yt,playlistId)
+
+def get_comments(yt,video_ids):
+    all_comments = []
+
+    for video_id in video_ids:
+        request = yt.commentThreads().list(
+            part="snippet,replies",
+            videoId=video_id
+        )
+        response = request.execute()
+
+        comments_in_video = [comment['snippet']['topLevelComment']['snippet']['textOriginal'] for comment in response['items']]
+        comments_in_video_info = {'video_id':video_id,'comments':comments_in_video}
+
+        all_comments.append(comments_in_video_info)
+    return pd.DataFrame(all_comments)
+
+comments_df = get_comments(yt,video_ids)
+comments_df.to_csv('Comments_Details.csv', index=False)
